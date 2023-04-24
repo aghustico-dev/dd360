2d65f6689ff937486b4d3f1067edf065f54f3ef4
aghustico <85595869+aghustico-dev@users.noreply.github.com>
2023-04-24 00:13:15 -0500

    envío de mi examen DD360

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "c:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\PRUEBA\\DD360-examen\\pruebatecnica" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=c:\\src\\flutter"
+  "PROJECT_DIR=D:\\PRUEBA\\DD360-examen\\pruebatecnica"
+  "FLUTTER_ROOT=c:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\PRUEBA\\DD360-examen\\pruebatecnica\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\PRUEBA\\DD360-examen\\pruebatecnica"
+  "FLUTTER_TARGET=D:\\PRUEBA\\DD360-examen\\pruebatecnica\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNWQyYzA1NmEwOTE0YTQ2YmJkMGI0NWNhYzRhZTIwYTAwODAyZjdjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\PRUEBA\\DD360-examen\\pruebatecnica\\.dart_tool\\package_config.json"
+)
